f8d6dbb3bceecae82cc6d13fc70b6cb4a64a994f
Dora Chin <chin22d@gmail.com>
2020-09-22 21:00:47 -0700

    Add a stationary rectangle, add a rotation when left arrow is held down

--- p5_test.js
+++ p5_test.js
@@ -4,8 +4,14 @@ function setup() {
 
 function draw() {
   background(220, 175, 200);
-  if (keyIsPressed === true) {
-    fill(0);
-    rotate(1);
+  // if (keyIsPressed === true) {
+  //   fill(0);
+  //   rotate(1);
+  // }
+  if (keyIsDown(LEFT_ARROW)) {
+    rotate(20);
+  } else {
+    fill(255);
   }
+  rect(25, 25, 50, 50);
 }
